7e342316f7552e67e3d597bbffecec62445cb527
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-02-19 04:00:05 -0300

    Add files via upload

---
+++ 45ex Repete 10x o que voce escreve.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+#45. Crie um programa que leia uma mensagem do teclado, em seguida, crie uma função que imprima esta mensagem 10x na tela.
+
+oi = input("Escreva algo: ")
+print()
+
+for c in range(0,10):
+    print(f'{oi}')
